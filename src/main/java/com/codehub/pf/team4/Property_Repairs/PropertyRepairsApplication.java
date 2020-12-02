87f03c25892a3e15f955658a08dbd30ffb7f8b3e
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-03 00:47:42 +0200

    Initial Project Commit + application.properties db config and server port

---
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/PropertyRepairsApplication.java
@@ -0,0 +1,13 @@
+package com.codehub.pf.team4.Property_Repairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRepairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyRepairsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehub/pf/team4/Property_Repairs/PropertyRepairsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehub.pf.team4.Property_Repairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRepairsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

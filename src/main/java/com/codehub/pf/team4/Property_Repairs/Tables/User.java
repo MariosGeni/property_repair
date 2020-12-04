b7e7a66ad9d0fe36c8fd06450851fc9f8a440294
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-04 18:37:28 +0200

    imports for users and user class

--- src/main/java/com/codehub/pf/team4/Property_Repairs/Tables/User.java
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/Tables/User.java
@@ -1,19 +1,30 @@
 package com.codehub.pf.team4.Property_Repairs.Tables;
 
 
+import com.codehub.pf.team4.Property_Repairs.enums.HouseType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "User", uniqueConstraints = {@UniqueConstraint(columnNames = {"firstname", "lastname"})})
+@Table(name = "Users", uniqueConstraints = {@UniqueConstraint(columnNames = {"afm"})})
+@Data
 public class User {
 
-    private static final int MAX_NAME_LENGHT = 50;
+    private static final int MAX_NAME_LENGTH = 50;
 
     @Id
-    @Column(name = "userId", nullable = false)
+    @Column(name = "user_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    @Column(name = "afm", length = 9) private Integer afm;
+    @Column(name = "first_name") private String firstName;
+    @Column(name = "last_name") private String lastName;
+    @Column(name = "address") private String address;
+    @Column(name = "phone_number", length = 10) private Integer phoneNumber;
+    @Column(name = "password") private String password;
+    @Enumerated(EnumType.STRING) @Column(name = "house_type") private HouseType houseType;
+    @Column(name = "email") private String email;
 }

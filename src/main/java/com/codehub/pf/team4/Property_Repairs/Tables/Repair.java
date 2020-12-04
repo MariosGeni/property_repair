d8c524037d684cfc3667f312b91b8cf9819e4a7f
DimitraFl <dimitraflor@gmail.com>
2020-12-04 18:25:38 +0200

    H2 changes

---
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/Tables/Repair.java
@@ -0,0 +1,49 @@
+package com.codehub.pf.team4.Property_Repairs.Tables;
+
+import com.codehub.pf.team4.Property_Repairs.enums.HouseType;
+import com.codehub.pf.team4.Property_Repairs.enums.RepairType;
+import com.codehub.pf.team4.Property_Repairs.enums.State;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Users")
+@Data
+public class Repair {
+    private static final int MAX_NAME_LENGTH = 50;
+
+    @Id
+    @Column(name = "repair_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private Timestamp date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    private State state;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "repair_type")
+    private RepairType repairType;
+
+    @Column(name = "cost")
+    private Long cost;
+
+    @Column(name = "address")
+    private String address;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "description")
+    private String description;
+
+    public Repair() {
+    }
+}
--- src/main/java/com/codehub/pf/team4/Property_Repairs/Tables/User.java
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/Tables/User.java
@@ -1,19 +1,48 @@
 package com.codehub.pf.team4.Property_Repairs.Tables;
-
+import com.codehub.pf.team4.Property_Repairs.enums.HouseType;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "User", uniqueConstraints = {@UniqueConstraint(columnNames = {"firstname", "lastname"})})
+@Table(name = "User", uniqueConstraints = {@UniqueConstraint(columnNames = {"afm"})})
+@Data
 public class User {
 
-    private static final int MAX_NAME_LENGHT = 50;
+    private static final int MAX_NAME_LENGTH = 50;
 
     @Id
-    @Column(name = "userId", nullable = false)
+    @Column(name = "user_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "afm", length = 9)
+    private Integer afm;
+
+    @Column(name = "first_name")
+    private  String firstName;
+
+    @Column(name = "last_name")
+    private  String lastName;
+
+    @Column(name = "address")
+    private  String address;
+
+    @Column(name = "phone_number", length=10)
+    private  Integer phoneNumber;
+
+    @Column(name = "password")
+    private  String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "property_type")
+    private HouseType propertyType;
+
+    @Column( name= "email")
+    private String email;
+
+    @OneToMany(mappedBy = "repair", targetEntity = Repair.class)
+    private List<Repair> repairs;
 
 }

8b72802ce605429c1afd12a04929a4580a734acb
Thanasis Galatis <gnasos219@gmail.com>
2020-12-05 14:24:40 +0200

    services structure

---
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/service/RepairService.java
@@ -0,0 +1,4 @@
+package com.codehub.pf.team4.Property_Repairs.service;
+
+public interface RepairService {
+}
--- src/main/java/com/codehub/pf/team4/Property_Repairs/service/UserService.java
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/service/UserService.java
@@ -1,4 +1,12 @@
 package com.codehub.pf.team4.Property_Repairs.service;
 
+import com.codehub.pf.team4.Property_Repairs.Tables.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+
+    Optional<User> fetchUserById(Long id);
+
 }

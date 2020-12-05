7858908806781ca4c5317458c88914f580687faa
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-05 13:55:02 +0200

    Created UserRepository and Updated Repairrepository

---
+++ src/main/java/com/codehub/pf/team4/Property_Repairs/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.codehub.pf.team4.Property_Repairs.repository;
+
+import com.codehub.pf.team4.Property_Repairs.Tables.Repair;
+import com.codehub.pf.team4.Property_Repairs.Tables.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByAfm(Integer afm);
+    Optional<User> findByEmail(String email);
+
+    /*@Query(value ="SELECT u FROM User u JOIN FETCH u.repairs WHERE u.afm = (:afm)")
+    List<User> findRepairsByAfm(@Param("afm") Integer afm);
+    AYTO PREPEI NA DOULEPSEI
+     */
+}

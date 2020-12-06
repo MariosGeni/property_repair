74201cd996714e4942758fc05e44b58fe0458153
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-06 16:11:44 +0200

    [main] - Updated AdminController - added endpoints.

--- src/main/java/com/codehub/pf/team4/controller/AdminController.java
+++ src/main/java/com/codehub/pf/team4/controller/AdminController.java
@@ -7,35 +7,77 @@ import com.codehub.pf.team4.service.RepairService;
 import com.codehub.pf.team4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/admin/")
+@RequestMapping("/admin")
 public class AdminController {
     @Autowired
-    private UserService userService;
+    UserService userService;
+
     @Autowired
-    private RepairService repairService;
+    RepairService repairService;
 
-    @GetMapping("home")
+    @GetMapping(value = "")
     @ResponseBody
-    public List<Repair> getAdminHome() {
-        return repairService.getAllRepairs();
+    public String getAdminHome() {
+        return "admin-home-view";
     }
 
-    @GetMapping("users")
+    @GetMapping(value = "/owners")
     @ResponseBody
-    public List<User> getUsers() {
-        return userService.getAllUsers();
+    public String getAdminOwnersPage() {
+        // --- owners showcase here --- //
+        return "admin-owners-view";
     }
 
-    @GetMapping("repairs")
+    @GetMapping(value = "/owners/search") // Search 'owner-user' by 'afm/email' queryString
     @ResponseBody
-    public List<Repair> getRepairs() {
-        return repairService.getAllRepairs();
+    public String getAdminSearchOwnerPage(@RequestParam(value = "afm", defaultValue = "") String afm,
+                                          @RequestParam(value = "email", defaultValue = "") String email) {
+        // --- search code here --- //
+        return "admin-owners-view";
+    }
+
+    @GetMapping(value = "/owners/edit/{id}") // Edit owner by its id
+    @ResponseBody
+    public String getAdminEditOwnersPage(@PathVariable("id") Long id, Model model) {
+        Optional<User> theOwner = userService.findUserById(id);
+
+        theOwner.ifPresent(owner -> { // if owner not null add to model
+            model.addAttribute("owner", owner);
+        });
+
+        return "edit-owners-view";
+    }
+
+    @GetMapping(value = "/repairs")
+    @ResponseBody
+    public String getAdminRepairsPage() {
+        // --- repairs showcase here --- //
+        return "admin-repairs-view";
+    }
+
+    @GetMapping(value = "/repairs/search") // Search 'repairs/user' by 'date' queryString
+    @ResponseBody
+    public String getAdminSearchRepairPage(@RequestParam(value = "date", defaultValue = "") String date) {
+        // --- search code here --- //
+        return "search-repair-view";
+    }
+
+    @GetMapping(value = "/repairs/edit/{id}") // Edit repair by its id
+    @ResponseBody
+    public String getAdminEditRepairsPage(@PathVariable("id") Long id, Model model) {
+        Optional<Repair> theRepair = repairService.getRepairById(id);
+
+        theRepair.ifPresent(repair -> { // if repair not null add to model
+            model.addAttribute("repair", repair);
+        });
+
+        return "edit-repair-view";
     }
 }

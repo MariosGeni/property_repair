15da6d8bb7bb7859dec869801af9a77f3cdbdd53
DimitraFl <dimitraflor@gmail.com>
2020-12-06 16:23:33 +0200

    Import.sql changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,7 +10,7 @@ INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, pas
 
 INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PAINTING', 130, 'Klemanso 25', 1, '2 Rooms');
 INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'INSULATION', 1500, 'Korai 47', 2, 'Whole House');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'FRAMES', 2500, '', 3, '9 Windows');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PLUMBING', 300, 'asdasd', 4, 'Bathroom');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'ELECTRICAL_WORK', 150, 'asdasd', 5, 'Electric Sockets');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'ELECTRICAL_WORK', 1000, 'asdasd', 6, 'New sockets & switches for the whole house');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'FRAMES', 2500, 'Skiathou 67', 3, '9 Windows');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PLUMBING', 300, 'Korai 47', 4, 'Bathroom');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'ELECTRICAL_WORK', 150, 'Venizelou 78', 5, 'Electric Sockets');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'ELECTRICAL_WORK', 1000, 'Kavalas 43', 6, 'New sockets & switches for the whole house');

311e3084f9f84db6d59c3a784b8723e0c0a05c2c
DimitraFl <dimitraflor@gmail.com>
2020-12-06 16:11:52 +0200

    Import.sql changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,16 @@
 ALTER TABLE Repair ALTER COLUMN address VARCHAR(255) COLLATE Latin1_General_100_CI_AI_SC_UTF8;
 
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456789, 'John', 'Smith', 'Kapou', 00000001, 'someone@somecompany.com', '123456789', 'DETACHED');
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456788, 'John', 'Smith', 'Kapou', 0000000, 'someone1@somecompany.com', '123456789', 'MAISONETTE');
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456787, 'John', 'Smith', 'Kapou', 0000000, 'someone2@somecompany.com', '123456', 'APARTMENT_BUILDING');
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456786, 'John', 'Smith', 'Kapou', 0000000, 'someone3@somecompany.com', '123456', 'DETACHED');
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456785, 'John', 'Smith', 'Kapou', 0000000, 'someone4@somecompany.com', '123456', 'DETACHED');
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456784, 'John', 'Smith', 'Kapou', 0000000, 'someone5@somecompany.com', '123654', 'DETACHED');
-INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456789, 'John', 'Smith', 'Kapou', 0000000, 'someone6@somecompany.com', '654123', 'DETACHED');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456789, 'John', 'Smith', 'Tsimiski 25', 2310444455, 'johnsmith@somecompany.com', '123456789', 'DETACHED_HOUSE');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (987654321, 'George', 'Papadopoulos', 'Trapezountos 3', 2102102100, 'georgepapadopoulos@somecompany.com', '123456789', 'MAISONETTE');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (112356794, 'Athanasios', 'Galatis', 'Kavalas 43', 2310231023, 'athanasiosgalatis2@somecompany.com', '123456', 'APARTMENT_BUILDING');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123457890, 'Marios', 'Genigiorgis', 'Korai 47', 2103456789, 'mariosgenigiorgis@somecompany.com', '123456', 'MAISONETTE');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123432109, 'Niyazi', 'Haci-Halil', 'Smirnis 2', 2541012345, 'niyazihacihalil@somecompany.com', '123456', 'DETACHED_HOUSE');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (987643246, 'Dimitra', 'Florou', 'Venizelou 78', 2541023456, 'dimitraflorou5@somecompany.com', '123654', 'APARTMENT_BUILDING');
+INSERT INTO Users (afm, first_name, last_name, address, phone_number, email, password, house_type) VALUES (123456700, 'Jonas', 'Kahnwald', 'Skiathou 67', 2310056789, 'johnaskahnwald@somecompany.com', '654123', 'MAISONETTE');
 
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PAINTING', 13, 'Κλεμανσώ 25', 1, 'a6s5d1a6s5d1a65sd');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'INSULATION', 15, 'asdasdasd', 2, '123456789');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'FRAMES', 25, 'asdasdasd', 3, 'asdasdasdasd');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PLUMBING', 3000000000, 'asdasd', 1, 'asdasd');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'ELECTRICAL_WORK', 15, 'asdasd', 2, '123456789');
-INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'ELECTRICAL_WORK', 25, 'asdasd', 3, 'asdasdasdasd');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PAINTING', 130, 'Klemanso 25', 1, '2 Rooms');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'INSULATION', 1500, 'Korai 47', 2, 'Whole House');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'FRAMES', 2500, '', 3, '9 Windows');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'WAITING', 'PLUMBING', 300, 'asdasd', 4, 'Bathroom');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'ONGOING', 'ELECTRICAL_WORK', 150, 'asdasd', 5, 'Electric Sockets');
+INSERT INTO Repair ("date", state, repair_type, cost, address, user_id, description) VALUES (getDate(), 'DONE', 'ELECTRICAL_WORK', 1000, 'asdasd', 6, 'New sockets & switches for the whole house');

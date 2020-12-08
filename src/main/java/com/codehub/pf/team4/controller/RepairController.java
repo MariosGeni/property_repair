a070ecfcee31dc1a6c0af0925d7a11ae3a61ab70
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-08 16:24:51 +0200

    [main] - added ownerController and splitted admin to owner admin and repair controllers

--- src/main/java/com/codehub/pf/team4/controller/RepairController.java
+++ src/main/java/com/codehub/pf/team4/controller/RepairController.java
@@ -1,31 +1,27 @@
 package com.codehub.pf.team4.controller;
 
 import com.codehub.pf.team4.domains.Repair;
-import com.codehub.pf.team4.enums.State;
-import com.codehub.pf.team4.repository.RepairRepository;
-import com.codehub.pf.team4.repository.UserRepository;
-import com.codehub.pf.team4.utils.DateProvider;
+import com.codehub.pf.team4.domains.User;
+import com.codehub.pf.team4.service.RepairService;
+import com.codehub.pf.team4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/admin")
 public class RepairController {
 
     @Autowired
-    private RepairRepository repairRepository;
+    UserService userService;
 
     @Autowired
-    private UserRepository userRepository;
+    RepairService repairService;
 
     @GetMapping("hello")
     public String hello(Model model) {
@@ -33,15 +29,45 @@ public class RepairController {
         return "hello";
     }
 
-    @GetMapping("repair")
+    @GetMapping
     @ResponseBody
     public List<Repair> getRepairs() {
-        return userRepository.findRepairsByAfm(123456789);
+        return userService.getRepairsByUserAfm(123456789);
     }
 
-    @GetMapping("repairsOfTheDay")
+    @GetMapping("repairs")
     @ResponseBody
-    public List<Repair> getRepairsOfTheDay() throws Exception {
-        return repairRepository.findByDateIsBetweenAndStateEquals(DateProvider.getEndOfDay(), DateProvider.getEndOfDay(), State.ONGOING);
+    public List<Repair> getAllRepairs() {
+        return repairService.getAllRepairs();
+    }
+
+    // *************************************************** //
+    // ======================== REPAIRS ================== //
+    // *************************************************** //
+
+    @GetMapping(value = "/repairs")
+    @ResponseBody
+    public String getAdminRepairsPage() {
+        // --- repairs showcase here --- //
+        return "admin-repairs-view";
+    }
+
+    @GetMapping(value = "/repairs/search") // Search 'repairs/user' by 'date' queryString
+    @ResponseBody
+    public String getAdminSearchRepairPage(@RequestParam(value = "date", defaultValue = "") String date) {
+        // --- search code here --- //
+        return "search-repair-view";
+    }
+
+    @GetMapping(value = "/repairs/edit/{id}") // Edit repair by its id
+    @ResponseBody
+    public String getAdminEditRepairsPage(@PathVariable("id") Long id, Model model) {
+        Optional<Repair> theRepair = repairService.getRepairById(id);
+
+        theRepair.ifPresent(repair -> { // if repair not null add to model
+            model.addAttribute("repair", repair);
+        });
+
+        return "edit-repair-view";
     }
 }

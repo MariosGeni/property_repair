82dd0bd59622f766bfdf26ec5b81a69f3a65d5cd
Thanasis Galatis <gnasos219@gmail.com>
2020-12-11 16:20:47 +0200

    [main] - added user model

---
+++ src/main/java/com/codehub/pf/team4/model/UserModel.java
@@ -0,0 +1,33 @@
+package com.codehub.pf.team4.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserModel {
+    private Long id;
+    private Integer afm;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private Long phoneNumber;
+    private String email;
+
+
+    public UserModel(Long id, String email, Integer afm) {
+        this.id = id;
+        this.email = email;
+        this.afm = afm;
+    }
+
+    public UserModel(Long id, Integer afm, String firstName, String lastName, String address, Long phoneNumber, String email) {
+        this.id = id;
+        this.afm = afm;
+        this.email = email;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}

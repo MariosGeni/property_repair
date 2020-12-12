8087d6649fb0601d0511e55666bec1a335f8b557
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-12 17:26:27 +0200

    [niazi] - Created RepairFormMapper class

--- src/main/java/com/codehub/pf/team4/utils/DateProvider.java
+++ src/main/java/com/codehub/pf/team4/utils/DateProvider.java
@@ -6,18 +6,27 @@ import java.util.Date;
 
 public abstract class DateProvider {
     public static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final SimpleDateFormat SDF_NO_TIME = new SimpleDateFormat("yyyy-MM-dd");
 
     public static Timestamp getStartOfDay() throws Exception{
-        String today = theDay() + " 09:00:00";
+        String today = theDayString() + " 09:00:00";
         return getFinalTime(SDF, today);
     }
 
     public static Timestamp getEndOfDay() throws Exception{
-        String today = theDay() + " 23:59:59";
+        String today = theDayString() + " 23:59:59";
         return getFinalTime(SDF, today);
     }
 
-    private static String theDay() {
+    public static Timestamp getDate(Timestamp date) throws Exception{
+        return new Timestamp(SDF_NO_TIME.parse(date.toString()).getTime());
+    }
+
+    public static boolean isToday(Timestamp date) {
+        return SDF_NO_TIME.format(date.getTime()).equals(theDayString());
+    }
+
+    private static String theDayString() {
         return new Timestamp(new Date().getTime()).toString().substring(0, 10);
     }
 

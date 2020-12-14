7a5928ab321a427702a33a882e8ee0df8af1ace1
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-14 14:50:06 +0200

    [niazi] - removed duplicate js and made use of jquery

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,29 @@
-$(document).ready(function() {
-    toggleUserSearch();
+$(function() { // <-- is short for $(document).ready(function() {});
+    $("#search-user-select").on("change", function(e) { toggleUserSearch(e); }); // same thing as doing directly from the element
+    // its better to call in js so you can see all events at once without having to go in the html and check for events
 });
 
-(function(){
-    console.log("Hello World!");
-})();
+//jQuery - its good to make use of jQuery because we implemented it.
+function toggleUserSearch(e) {
+    var $target = $(e.target);
+    let $afmDiv = $("#afm-div");
+    let $emailDiv = $("#email-div");
 
-function toggleUserSearch() {
+    // toggle visibility
+    // can be written in less lines but kept it readable
+    if ($target.attr("value") == "email") {
+        $afmDiv.css({ display: "none" });
+        $emailDiv.css({ display: "block" });
+    }
+
+    if($target.attr("value") == "afm") {
+        $emailDiv.css({ display: "none" });
+        $afmDiv.css({ display: "block" });
+    }
+}
+
+// vanilla js
+/*function toggleUserSearch() {
     let afmDiv = document.getElementById("afm-div");
     let emailDiv = document.getElementById("email-div");
     // toggle visibility
@@ -18,4 +35,4 @@ function toggleUserSearch() {
         afmDiv.style.display = "none";
         emailDiv.style.display = "block";
     }
-}
+}*/

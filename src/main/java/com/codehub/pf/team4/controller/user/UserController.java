8f37227a4170a20a0e4564ef2c4e33c15d8ae01b
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-17 22:55:56 +0200

    [niazi] -added username model attreibute for user only

--- src/main/java/com/codehub/pf/team4/controller/user/UserController.java
+++ src/main/java/com/codehub/pf/team4/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.codehub.pf.team4.domains.User;
 import com.codehub.pf.team4.repository.UserRepository;
 import com.codehub.pf.team4.service.RepairService;
 import com.codehub.pf.team4.service.UserService;
+import com.codehub.pf.team4.utils.StatusProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +28,7 @@ public class UserController {
     @GetMapping("home")
     public String getHomePage(Model model) {
         model.addAttribute("repairs", userService.getRepairsByUserId(1L));
+        model.addAttribute("userName", userService.findUserByEmail(StatusProvider.getLoggedInEmail()).get().getFirstName());
         return "pages/home";
     }
 }

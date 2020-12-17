e7cae2ed18119d3846978aad9c4bfed4a6f7e06f
Niazi Chatzi Chalil <Niyazi_Hacihalil@hotmail.com>
2020-12-17 21:06:51 +0200

    [niazi] - added methods to StatusProvider

--- src/main/java/com/codehub/pf/team4/utils/StatusProvider.java
+++ src/main/java/com/codehub/pf/team4/utils/StatusProvider.java
@@ -1,13 +1,15 @@
 package com.codehub.pf.team4.utils;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class StatusProvider {
 
     public static boolean isLoggedIn(){
-        return false;
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        return user.equals("anonymousUser")? false : true;
     }
 
     public static String getLoggedInEmail(){
-
-        return null;
+        return isLoggedIn()? SecurityContextHolder.getContext().getAuthentication().getName() : "";
     }
 }
